a350a614e536fc9f1547568ccdae6364931e5586
Osman Gokalp <osmangokalp11@gmail.com>
2017-06-22 17:48:09 +0300

    DE_Sensor added

--- public_html/DifferentialEvolution.js
+++ public_html/DifferentialEvolution.js
@@ -5,21 +5,7 @@ importScripts('./BenchmarkFunctions.js', './Solution.js');
 /**
  * Basic DE algorithm (DE/rand/1)
  */
-function DiferentialEvolution() {
-    this.NP = null; //the number of colony size
-    this.CR = null;
-    this.F = null;
-    this.maxNumOfFunctionEval = null;
-    this.objFunc = null;
-    this.upperBound = null;
-    this.lowerBound = null;
-    this.dimension = null;
-    this.solutions = null;
-    this.globalBest = null;
-}
-;
-
-DiferentialEvolution.prototype.init = function (parameters) {
+function DifferentialEvolution(parameters) {
     this.NP = parameters.NP;
     this.F = parameters.F;
     this.CR = parameters.CR;
@@ -29,71 +15,62 @@ DiferentialEvolution.prototype.init = function (parameters) {
     this.lowerBound = parameters.lowerBound;
     this.dimension = parameters.dimension;
     this.solutions = [];
-};
+    
+    this.globalBest = null;
+}
+;
 
-DiferentialEvolution.prototype.solve = function () {
+DifferentialEvolution.prototype.solve = function () {
     var numOfFunctionEval = 0;
-    var i, rA, rB, rC, R, rI, min = Number.MAX_VALUE, minIndex = 0;;
+    var i, rA, rB, rC, R, rI, min = Number.MAX_VALUE, minIndex = 0;
     var a = [], b = [], c = [], y = [], x = [];
 
-    //initialization step
-    for (i = 0; i < this.NP; i++) {
-        var randPos = createRandomPosition(this.lowerBound, this.upperBound, this.dimension);
-        var randF = this.objFunc(randPos);
-        this.solutions[i] = new Solution(randPos, randF); //create random solution
-
-        if (randF < min) {
-            min = randF;
-            minIndex = i;
-        }
-    }
-    this.globalBest = new Solution(this.solutions[minIndex].position.slice(0), min);
+    this.createInitialPopulation();
 
     while (numOfFunctionEval + this.NP <= this.maxNumOfFunctionEval) {
         for (i = 0; i < this.NP; i++) {
             //Pick three agents a, b and c from the population at random
             do {
                 rA = Math.floor((Math.random() * this.NP));
-            } while (rA == i);
+            } while (rA === i);
 
             a = this.solutions[rA].position;
 
             do {
                 rB = Math.floor((Math.random() * this.NP));
-            } while (rB == i || rB == rA);
+            } while (rB === i || rB === rA);
 
             b = this.solutions[rB].position;
 
             do {
                 rC = Math.floor((Math.random() * this.NP));
-            } while (rC == i || rC == rA || rC == rB);
+            } while (rC === i || rC === rA || rC === rB);
 
             c = this.solutions[rC].position;
 
             R = Math.floor((Math.random() * this.dimension)); //Pick a random dimension index R 
 
             x = this.solutions[i].position; // current solution
-
+            
+            console.log("rA: " + rA + ", rB: " + rB + ", rC: " + rC + ", i: " + i);
+            
             y = x.slice(0);
             for (var j = 0; j < this.dimension; j++) {
                 rI = Math.random();
-                if (rI < this.CR || j == R) {
+                if (rI < this.CR || j === R) {
                     y[j] = a[j] + this.F * (b[j] - c[j]);
-                    if (y[j] < this.lowerBound) {//fix boundary
-                        y[j] = this.lowerBound;
-                    } else if (y[j] > this.upperBound) {
-                        y[j] = this.upperBound;
-                    }
                 } else {
                     y[j] = x[j];
                 }
             }
 
-            var fy = this.objFunc(y);
+            y = this.fixBoundary(y);
+            var fy = this.calculateObjValue(y);
+
             if (fy < this.solutions[i].fitness) {
                 this.solutions[i].position = y.slice(0);
                 this.solutions[i].fitness = fy;
-
+               
                 if (fy < this.globalBest.fitness) {
                     this.globalBest.solution = y.slice(0);
                     this.globalBest.fitness = fy;
@@ -103,6 +80,43 @@ DiferentialEvolution.prototype.solve = function () {
         }
         numOfFunctionEval += this.NP;
     }
+};
+
+DifferentialEvolution.prototype.calculateObjValue = function (array) {
+    return this.objFunc(array);
+};
+
+DifferentialEvolution.prototype.createRandomSolution = function () {
+    var randPos = createRandomPosition(this.lowerBound, this.upperBound, this.dimension);
+    return randPos;
+};
+
+DifferentialEvolution.prototype.fixBoundary = function (array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > this.upperBound) {
+            array[i] = this.upperBound;
+        } else if (array[i] < this.lowerBound) {
+            array[i] = this.lowerBound;
+        }
+    }
+    
+    return array;
+};
+
+DifferentialEvolution.prototype.createInitialPopulation = function () {
+    var i, min = Number.MAX_VALUE, minIndex = 0;;
+    //initialization step
+    for (var i = 0; i < this.NP; i++) {
+        var randPos = createRandomPosition(this.lowerBound, this.upperBound, this.dimension);
+        var randF = this.calculateObjValue(randPos);
+        this.solutions[i] = new Solution(randPos, randF); //create random solution
+
+        if (randF < min) {
+            min = randF;
+            minIndex = i;
+        }
+    }
+    this.globalBest = new Solution(this.solutions[minIndex].position.slice(0), min);
 };
 
 onmessage = function (e) {
@@ -126,7 +140,6 @@ onmessage = function (e) {
         "dimension": e.data[7]
     };
 
-    var de = new DiferentialEvolution();
-    de.init(parameters);
+    var de = new DifferentialEvolution(parameters);
     de.solve();
 };
---
+++ public_html/DifferentialEvolution_Sensor.js
@@ -0,0 +1,81 @@
+"use strict";
+
+importScripts('./DifferentialEvolution.js', './SensorDeploymentProblem.js', './Solution.js');
+
+function DifferentialEvolution_Sensor(parameters) {
+    DifferentialEvolution.call(this, parameters);
+    this.sensorDeploymentProblem = new SensorDeploymentProblem(parameters.numOfSensors, parameters.numOfPoints,
+            parameters.radius, parameters.width, parameters.height);
+    this.sensorDeploymentProblem.setPointsArray(parameters.initialPointsArray);
+}
+;
+
+DifferentialEvolution_Sensor.prototype = Object.create(DifferentialEvolution.prototype);
+DifferentialEvolution_Sensor.prototype.constructor = DifferentialEvolution;
+
+DifferentialEvolution_Sensor.prototype.calculateObjValue = function (array) {
+    this.sensorDeploymentProblem.setSensorsArray(array);
+    return -1 * this.sensorDeploymentProblem.getCoverage();
+};
+
+DifferentialEvolution_Sensor.prototype.createRandomSolution = function () {
+    var randPos = this.sensorDeploymentProblem.generateRandomSensorsArray();
+    return randPos;
+};
+
+DifferentialEvolution_Sensor.prototype.createInitialPopulation = function () {
+    var i, min = Number.MAX_VALUE, minIndex = 0;
+    ;
+    //initialization step
+    for (var i = 0; i < this.NP; i++) {
+        var randPos = this.sensorDeploymentProblem.generateRandomSensorsArray();
+        var randF = this.calculateObjValue(randPos);
+        this.solutions[i] = new Solution(randPos, randF); //create random solution
+
+        if (randF < min) {
+            min = randF;
+            minIndex = i;
+        }
+    }
+    this.globalBest = new Solution(this.solutions[minIndex].position.slice(0), min);
+};
+
+DifferentialEvolution_Sensor.prototype.fixBoundary = function (array) {
+    for (var i = 0; i < this.sensorDeploymentProblem.getNumOfSensors(); i++) {
+        if (array[2 * i] > this.sensorDeploymentProblem.getAreaWidth()) {
+            array[2 * i] = this.sensorDeploymentProblem.getAreaWidth();
+        } else if (array[2 * i] < 0) {
+            array[2 * i] = 0;
+        }
+
+        if (array[2 * i + 1] > this.sensorDeploymentProblem.getAreaHeight()) {
+            array[2 * i + 1] = this.sensorDeploymentProblem.getAreaHeight();
+        } else if (array[2 * i + 1] < 0) {
+            array[2 * i + 1] = 0;
+        }
+    }
+
+    return array;
+};
+
+onmessage = function (e) {
+
+    var parameters = {
+        "NP": e.data[0],
+        "CR": e.data[1],
+        "F": e.data[2],
+        "maxNumOfFunctionEval": e.data[3],
+        "upperBound": e.data[4],
+        "lowerBound": e.data[5],
+        "dimension": e.data[6],
+        "numOfSensors": e.data[7],
+        "numOfPoints": e.data[8],
+        "radius": e.data[9],
+        "width": e.data[10],
+        "height": e.data[11],
+        "initialPointsArray": e.data[12]
+    };
+
+    var de_s = new DifferentialEvolution_Sensor(parameters);
+    de_s.solve();
+};
